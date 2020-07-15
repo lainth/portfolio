56ace10a4a6f4b01ac61991401548682c279841e
Thomas <thomas.laine25@gmail.com>
2020-07-15 16:18:01 +0200

    render Projects component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Presentation from './components/Presentation';
+import Projects from './components/Projects';
 
 import './App.scss';
 
@@ -14,6 +15,7 @@ const App = () => {
       <Header />
       <AboutMe />
       <Presentation />
+      <Projects />
     </Container>
   );
 }
