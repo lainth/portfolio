09cc4b5e3ab9a767cbfe7b321a8ad6271a28f5ce
Thomas <thomas.laine25@gmail.com>
2020-07-17 18:38:09 +0200

    render Techno component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Presentation from './components/Presentation';
 import Projects from './components/Projects';
+import Techno from './components/Techno';
 import Footer from './components/Footer';
 
 import './App.scss';
@@ -17,6 +18,7 @@ const App = () => {
       <AboutMe />
       <Presentation />
       <Projects />
+      <Techno />
       <Footer />
     </Container>
   );
