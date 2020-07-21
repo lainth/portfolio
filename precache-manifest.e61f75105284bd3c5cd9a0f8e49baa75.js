8cfcf1daf25b5464407b1f49e97618b34ff784d0
Thomas <thomas.laine25@gmail.com>
2020-07-21 14:00:48 +0200

    Updates

---
+++ precache-manifest.e61f75105284bd3c5cd9a0f8e49baa75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5db1c61e2128911ead0ea291a9283d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "46152c06ab687ed9f319",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e2a93d352aef256b8e0",
+    "url": "/portfolio/static/css/main.050da2bd.chunk.css"
+  },
+  {
+    "revision": "46152c06ab687ed9f319",
+    "url": "/portfolio/static/js/2.6ab6e72b.chunk.js"
+  },
+  {
+    "revision": "0d9b363eea25cd01c3fabb1e4fd4c4ef",
+    "url": "/portfolio/static/js/2.6ab6e72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2a93d352aef256b8e0",
+    "url": "/portfolio/static/js/main.473205e6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
