66c3c9ba21cb1e7b69dc4eba1972a3c9c30f7c99
Thomas <thomas.laine25@gmail.com>
2020-07-21 13:27:27 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c1905c2c1d824f07d88bf0c7df5c088e.js"
+  "/portfolio/precache-manifest.e61f75105284bd3c5cd9a0f8e49baa75.js"
 );
 
 self.addEventListener('message', (event) => {
