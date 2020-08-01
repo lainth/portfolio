56768aa868800402a8a25bb82050d1483e4fe367
Thomas <thomas.laine25@gmail.com>
2020-08-01 15:44:00 +0200

    changed text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
+import MyParticles from './Particles';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Typist from 'react-typist';
 
 const AboutMe = () => {
     return (
-        <Row id="about" name="about" className="about-wrapper">
-            <Col className="about-text">
+        <Row id="about" name="about" className="about-wrapper">  
+        <MyParticles />          
+      <Col className="about-text">
                 <div>
                     <Typist cursor={{hideWhenDone: true}}>
                         <span>Welcome.</span>
                         <Typist.Delay ms={1000} />
                         <br />
-                        <span>I'm Thomas, a web developer from France.</span>
+                        <span>I'm Thomas, a self-taught web developer from France.</span>
                         <Typist.Delay ms={1000} />
                         <br />
                         <span>I enjoy solving problems.</span>
