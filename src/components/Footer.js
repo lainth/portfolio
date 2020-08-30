baea14e8958c999e0e23473cff51728f839c2cc9
Thomas <thomas.laine25@gmail.com>
2020-08-30 12:02:29 +0200

    Added CV Button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,30 @@ import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 
+import Pdf from '../files/resume.pdf';
+import styled from "styled-components";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const popover = (
   <Popover id="popover-mail">
-    <Popover.Content>xxxx.xxxx@xxxx.xxx</Popover.Content>
+    <Popover.Content>thomas.laine25@gmail.com</Popover.Content>
   </Popover>
 );
+
+const StyledA = styled.a`
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    color: white;
+    text-decoration: none;
+  }
+
+`;
+
 
 const Footer = () => {
   return (
@@ -23,7 +38,7 @@ const Footer = () => {
       <Col xs={12}>
         <p className="footer-title">Want to get in touch?</p>
       </Col>
-      <Col xs={12} sm={4} className="text-center">
+      <Col xs={12} sm={3} className="text-center">
         <Nav className="mr-auto flex-column">
           <Nav.Link
             href="https://github.com/lainth"
@@ -34,7 +49,7 @@ const Footer = () => {
           </Nav.Link>
         </Nav>
       </Col>
-      <Col xs={12} sm={4} className="text-center">
+      <Col xs={12} sm={3} className="text-center">
         <Nav className="mr-auto flex-column">
           <Nav.Link
             href="https://twitter.com/ilainth"
@@ -45,10 +60,17 @@ const Footer = () => {
           </Nav.Link>
         </Nav>
       </Col>
-      <Col xs={12} sm={4} className="text-center">
+      <Col xs={12} sm={3} className="text-center">
         <OverlayTrigger trigger="click" placement="top" overlay={popover}>
           <Button className="footer-button">Email Me!</Button>
         </OverlayTrigger>
+      </Col>
+      <Col xs={12} sm={3} className="text-center">
+          <Button className="footer-cv-button">
+          <StyledA  href={Pdf} target="_blank" rel="noopener noreferrer">
+             My CV Here!
+          </StyledA >
+          </Button>
       </Col>
       <Col xs={12} className="text-center footer-copy">
         <div>
